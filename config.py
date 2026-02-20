f7db7a715d0955e5f52c9e2df2daaec7aa956aab
42Wor <wwork4287@gmail.com>
2026-02-20 10:21:37 +0500

    a

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     
     # MAAZDB PROJECT SETTINGS
-    LATEST_VERSION = "12.0.0"
+    LATEST_VERSION = "12.4.1"
     
     # Construct filenames dynamically based on version
     INSTALLER_WIN = f"MaazDB_Setup_Windows_x64_{LATEST_VERSION.replace('.', '')}.exe"
